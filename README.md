3d0d3c8f72b4e85135d85240569c2ccdc3fed1bb
Tom <tgriffith@jccap.org>
2021-02-21 15:31:16 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ResponseLinkAlertOneAssessment
+ Developer Skills Assessment Exercise for ResponseLink/Alert1.  Prompt received Feb 26, 2021.
---
+++ ResponseLinkAlertOneAssessment/ClientApp/src/app/cart/cart.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-cart',
+    templateUrl: './cart.component.html',
+    styleUrls: ['./cart.component.css']
+})
+/** cart component*/
+export class CartComponent {
+    /** cart ctor */
+    constructor() {
+
+    }
+}
