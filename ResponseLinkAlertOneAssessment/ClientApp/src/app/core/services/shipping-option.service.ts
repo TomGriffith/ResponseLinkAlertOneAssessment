fbbd90177d4b96919173faa499c0bf37a8c5749d
Tom <tgriffith@jccap.org>
2021-02-24 17:41:13 -0500

    Create shipping-option.service.ts

---
+++ ResponseLinkAlertOneAssessment/ClientApp/src/app/core/services/shipping-option.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ShippingOptionService {
+  constructor() {
+
+  }
+
+  // this would normally pull from a database
+  // but hard coded for this example
+  static getShippingOptions() {
+    return [
+      { cost: 0.00, id: "1", description: "1 to 2 Weeks" },
+      { cost: 5.99, id: "2", description: "Two Day" },
+      { cost: 9.99, id: "3", description: "Next Day" }
+    ];
+  }
+
+  // this would normally select from a database but
+  // I use .find to get it from the hard coded
+  // examples instead
+  static getShippingOption(id: string) {
+    return this.getShippingOptions()
+      .find(s => s.id === id);
+  }
+}
+
+export interface IShippingOption {
+  id: string;
+  cost: number;
+  description: string;
+}
